8a1c3c7d01816baec0eefa67310630006ba3f510
Bailey Cage <bailey.cage@gmail.com>
2022-09-21 11:21:55 -0400

    commit tree e13d43f3b242529ab8002b2fe5fcb0d79a6a9ca6

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  # your code here
+
+  def initialize (args)
+    args.keys.each do |key|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", args[key])
+    end
+  end
+
 end
